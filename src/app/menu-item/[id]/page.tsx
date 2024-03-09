9d60b8c8eb5184fc69a5238adca88899625b6a64
Michael Lei <michaelleie0ca@gmail.com>
2024-03-09 14:41:58 -0800

    Move menu data out of page

--- src/app/menu-item/[id]/page.tsx
+++ src/app/menu-item/[id]/page.tsx
@@ -1,8 +1,7 @@
 import Avatar from "@/components/design/avatar";
 import DynamicIsland from "@/components/design/dynamic-island";
 import { Headline } from "@/components/design/typography";
-import SandwichIcon from "@/components/icons/SandwichIcon";
-import SoupIcon from "@/components/icons/SoupIcon";
+
 import { Button } from "@/components/ui/button";
 
 import {
@@ -14,36 +13,15 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { menu } from "@/data/data";
 import { notFound } from "next/navigation";
-
-const specials = [
-  {
-    id: "soup",
-    icon: <SoupIcon width={311} height={215} />,
-    reserveIcon: <SoupIcon width={173.14} height={119.7} />,
-    description: `Discover the warmth of my homemade tomato soup, a comforting blend
-              of ripe tomatoes, fragrant herbs, and a hint of sweetness!`,
-    food: "Fresh Tomato Soup",
-    contains: ["Milk", "Gluten"],
-    mayContain: ["Nuts"],
-  },
-  {
-    id: "sandwich",
-    icon: <SandwichIcon width={222.57} height={222.57} />,
-    reserveIcon: <SandwichIcon width={140} height={140} />,
-    description: `Delight in my Belt Sandwich—a symphony of crispy bacon, fresh eggs, and vibrant veggies hugged by golden toast. Experience a burst of flavors curated for the ultimate satisfaction in every bite.`,
-    food: "BLT Sandwich",
-    contains: ["Milk", "Gluten"],
-    mayContain: ["Nuts", "Soy"],
-  },
-];
 
 export default function MenuItemPage({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  const special = specials.find((special) => special.id === id);
+  const special = menu.find((item) => item.id === id);
   if (!special) return notFound();
   return (
     <div>
