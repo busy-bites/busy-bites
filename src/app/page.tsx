6867ed1e6029bfa571d97e7cdf91e2ae9db7174a
scottchen98 <scott.chen1117@gmail.com>
2024-03-09 09:28:25 -0800

    add headline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Headline } from "@/components/design/typography";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <main className="mx-auto min-h-dvh max-w-[375px] bg-[#FFFDFB] px-4 pt-14">
+      <Headline> Good Appetite, Sofia</Headline>
+    </main>
+  );
 }
