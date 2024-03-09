62b278a4ba1803999dd445da370468cbeee39882
Michael Lei <michaelleie0ca@gmail.com>
2024-03-09 14:38:04 -0800

    Finish making menu page

--- src/app/menu-item/[id]/page.tsx
+++ src/app/menu-item/[id]/page.tsx
@@ -1,6 +1,7 @@
 import Avatar from "@/components/design/avatar";
 import DynamicIsland from "@/components/design/dynamic-island";
 import { Headline } from "@/components/design/typography";
+import SandwichIcon from "@/components/icons/SandwichIcon";
 import SoupIcon from "@/components/icons/SoupIcon";
 import { Button } from "@/components/ui/button";
 
@@ -13,19 +14,48 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { notFound } from "next/navigation";
 
-export default function MenuItemPage() {
+const specials = [
+  {
+    id: "soup",
+    icon: <SoupIcon width={311} height={215} />,
+    reserveIcon: <SoupIcon width={173.14} height={119.7} />,
+    description: `Discover the warmth of my homemade tomato soup, a comforting blend
+              of ripe tomatoes, fragrant herbs, and a hint of sweetness!`,
+    food: "Fresh Tomato Soup",
+    contains: ["Milk", "Gluten"],
+    mayContain: ["Nuts"],
+  },
+  {
+    id: "sandwich",
+    icon: <SandwichIcon width={222.57} height={222.57} />,
+    reserveIcon: <SandwichIcon width={140} height={140} />,
+    description: `Delight in my Belt Sandwich—a symphony of crispy bacon, fresh eggs, and vibrant veggies hugged by golden toast. Experience a burst of flavors curated for the ultimate satisfaction in every bite.`,
+    food: "BLT Sandwich",
+    contains: ["Milk", "Gluten"],
+    mayContain: ["Nuts", "Soy"],
+  },
+];
+
+export default function MenuItemPage({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const special = specials.find((special) => special.id === id);
+  if (!special) return notFound();
   return (
     <div>
       <DynamicIsland>
         <div className="mt-5 flex w-full flex-col items-center">
           <Headline className="mb-1">Today&apos;s Special</Headline>
           <span className="text-gray-600">Host: Jenny | 1 km away</span>
-          <SoupIcon width={311} height={215} />
-          <p className="text-xl font-bold">Fresh Tomato Soup</p>
+          {special.icon}
+          <p className="text-xl font-bold">{special.food}</p>
           <div className="mt-2 text-sm">
-            <p>Contain: Milk, Gluten</p>
-            <p>May Contain: Nuts</p>
+            <p>Contain: {special.contains.join(", ")}</p>
+            <p>May Contain: {special.mayContain.join(", ")}</p>
           </div>
         </div>
       </DynamicIsland>
@@ -33,10 +63,7 @@ export default function MenuItemPage() {
         <div className="my-10 flex flex-1 items-end gap-2 px-2">
           <Avatar />
           <div className="rounded-3xl border border-secondary bg-accent px-2 py-4">
-            <p className="p-2 text-sm font-semibold">
-              Discover the warmth of my homemade tomato soup, a comforting blend
-              of ripe tomatoes, fragrant herbs, and a hint of sweetness!
-            </p>
+            <p className="p-2 text-sm font-semibold">{special.description}</p>
           </div>
         </div>
         <div className="mt-20 px-2">
@@ -47,10 +74,10 @@ export default function MenuItemPage() {
               </Button>
             </DialogTrigger>
             <DialogContent className="max-w-md border-0 bg-secondary">
-              <div className="absolute -top-10 left-12 mx-auto w-fit translate-x-1/2 rounded-3xl bg-accent drop-shadow-md">
-                <SoupIcon width={173.14} height={119.7} />
-              </div>
-              <DialogHeader className="mx-auto mt-20 max-w-xs">
+              <DialogHeader className="mx-auto max-w-xs">
+                <div className=" mx-auto w-fit rounded-3xl bg-accent drop-shadow-md">
+                  {special.reserveIcon}
+                </div>
                 <div className="py-5">
                   <DialogTitle className="text-center text-xl font-bold">
                     You have successfully reserved your meal!
--- src/components/icons/SandwichIcon.tsx
+++ src/components/icons/SandwichIcon.tsx
@@ -1,13 +1,21 @@
 import Image from "next/image";
 
-export default function SandwichIcon({ className }: { className?: string }) {
+export default function SandwichIcon({
+  className,
+  width,
+  height,
+}: {
+  className?: string;
+  width: number;
+  height: number;
+}) {
   return (
     <Image
       className={className}
       src="/sandwich.svg"
       alt="Sandwich"
-      width={55}
-      height={55}
+      width={width}
+      height={height}
     />
   );
 }
