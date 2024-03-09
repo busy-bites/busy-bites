ed275ff42f275620be07c6351598c6b347b4817f
scottchen98 <scott.chen1117@gmail.com>
2024-03-09 15:31:53 -0800

    add markers to map

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,9 +1,15 @@
 "use client";
 
-import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import { icon } from "leaflet";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 import { useRef } from "react";
+
+const ICON = icon({
+  iconUrl: "/marker.png",
+  iconSize: [32, 32],
+});
 
 export default function MapPage() {
   const mapRef = useRef(null);
@@ -13,11 +19,13 @@ export default function MapPage() {
   return (
     <MapContainer
       center={[latitude, longitude]}
-      zoom={15}
+      zoom={17}
       ref={mapRef}
       style={{ height: "calc(100dvh - 56px)", width: "100%" }}
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <Marker position={[latitude, longitude]} icon={ICON}></Marker>
+      <Marker position={[49.252, -123.0019]} icon={ICON}></Marker>
     </MapContainer>
   );
 }
