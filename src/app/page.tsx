8d35fc22f51d5c1262d2fd74b55f1ce82ff7082a
scottchen98 <scott.chen1117@gmail.com>
2024-03-09 10:27:44 -0800

    add today's special section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import { Headline } from "@/components/design/typography";
+import AccentCard from "@/components/design/accent-card";
+import { Headline, Title } from "@/components/design/typography";
+import SoupIcon from "@/components/icons/SoupIcon";
 
 export default function Home() {
   return (
     <main className="mx-auto min-h-dvh max-w-[375px] bg-[#FFFDFB] px-4 pt-14">
       <Headline> Good Appetite, Sofia</Headline>
+      <Title>Today&apos;s Special</Title>
+      <AccentCard
+        image={<SoupIcon width={147.8} height={101.63} />}
+        title="Fresh Tomato Soup"
+        description="Jenny | Reserve By 3pm "
+      />
     </main>
   );
 }
