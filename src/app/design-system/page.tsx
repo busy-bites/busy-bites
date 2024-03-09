81d5ddd2e8b9b84dd729aac1e0620fc36f491c28
Michael Lei <michaelleie0ca@gmail.com>
2024-03-09 01:35:31 -0800

    Finish button ctas

--- src/app/design-system/page.tsx
+++ src/app/design-system/page.tsx
@@ -1,8 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 export default function DesignSystem() {
   return (
-    <main>
-      <h1>Design System</h1>
-      <p>Design system page</p>
+    <main className="mx-auto max-w-sm">
+      {/* Button section */}
+      <section className="flex gap-5 p-5">
+        <Button>Button CTA</Button>
+        <Button variant="outline">Button CTA</Button>
+      </section>
+      <section></section>
     </main>
   );
 }
