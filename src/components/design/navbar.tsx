775e55db8122d0e3926f4159c8d233409a6dc4f2
Michael Lei <michaelleie0ca@gmail.com>
2024-03-09 22:05:10 -0800

    Add menu color to indicate which page you are on

--- src/components/design/navbar.tsx
+++ src/components/design/navbar.tsx
@@ -1,30 +1,53 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import { Home, Map, ShoppingCart, Smile } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathname = usePathname();
   return (
     <nav className="fixed bottom-0  z-10 w-full justify-center">
       <ul className="mx-auto flex w-full max-w-md justify-evenly gap-5 border-t bg-[#FFFDFB] py-4">
         <li>
           <Link href="/">
-            <Home className="stroke-gray-500 hover:stroke-black" />
+            <Home
+              className={cn({
+                "stroke-gray-500 hover:stroke-black": pathname !== "/",
+                "stroke-secondary": pathname === "/",
+              })}
+            />
           </Link>
         </li>
         <li>
           <Link href="/map">
-            <Map className="stroke-gray-500 hover:stroke-black" />
+            <Map
+              className={cn({
+                "stroke-gray-500 hover:stroke-black": pathname !== "/map",
+                "stroke-secondary": pathname === "/map",
+              })}
+            />
           </Link>
         </li>
         <li>
           <Link href="/order">
-            <ShoppingCart className="stroke-gray-500 hover:stroke-black" />
+            <ShoppingCart
+              className={cn({
+                "stroke-gray-500 hover:stroke-black": pathname !== "/order",
+                "stroke-secondary": pathname === "/order",
+              })}
+            />
           </Link>
         </li>
         <li>
           <Link href="/profile">
-            <Smile className="stroke-gray-500 hover:stroke-black" />
+            <Smile
+              className={cn({
+                "stroke-gray-500 hover:stroke-black": pathname !== "/profile",
+                "stroke-secondary": pathname === "/profile",
+              })}
+            />
           </Link>
         </li>
       </ul>
