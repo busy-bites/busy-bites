8e22dffe3d0ca08a23bb35c903dbfe359f7ec49f
Michael Lei <michaelleie0ca@gmail.com>
2024-03-09 16:37:57 -0800

    Add sender and receiver chat component
    Co-authored-by: Jerry Fan <Jerryfan71@gmail.com>

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -4,34 +4,48 @@ import Avatar from "@/components/design/avatar";
 import { Smile, X, Image, ChevronRight } from "lucide-react";
 import DynamicIsland from "@/components/design/dynamic-island";
 import { Button } from "@/components/ui/button";
+import { FormEvent, useState } from "react";
+
+type ChatMessage = {
+  sender: string;
+  message: string;
+};
+
+const chatMessages: ChatMessage[] = [
+  {
+    sender: "Sophia",
+    message: "Hello, Jenny. I'll be there in 5 minutes!",
+  },
+  {
+    sender: "Jenny",
+    message: "Perfect! did you bring your own container?",
+  },
+  {
+    sender: "Sophia",
+    message: "Yes I did.",
+  },
+  {
+    sender: "Sophia",
+    message: "Thanks for making the food btw!",
+  },
+];
 
 export default function ChatPage() {
-  // have a sendMessage in a array of messages
-  const senderMessages = [
-    {
-      sender: "Sophia",
-      message: "Hello, Jenny. I'll be there in 5 minutes!",
-    },
-    {
-      sender: "Sophia",
-      message: "Yes I did.",
-    },
-    {
-      sender: "Sophia",
-      message: "Thanks for making the food btw!",
-    },
-  ];
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState(chatMessages);
+  const isSender = (sender: string) => sender === "Sophia";
 
-  const receiverMessages = [
-    {
-      sender: "Jenny",
-      message: "Perfect! did you bring your own container?",
-    },
-    {
-      sender: "Jenny",
-      message: "....",
-    },
-  ];
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setMessages([
+      ...messages,
+      {
+        sender: "Sophia",
+        message: message,
+      },
+    ]);
+    setMessage("");
+  };
 
   return (
     <div className="mx-auto max-w-sm space-y-10">
@@ -55,37 +69,26 @@ export default function ChatPage() {
       {/* Chat section */}
       <section className="flex flex-col gap-4">
         <div className="flex flex-col gap-2">
-          <div className="flex items-center justify-end">
-            {/* // map through senderMessages */}
-            {senderMessages.map((message, index) => (
-              <div
-                key={message.sender}
-                className="flex items-end justify-start"
-              >
-                <div className="max-w-xs break-words rounded-[20px] rounded-tl-none bg-amber-200 p-3 md:max-w-md lg:max-w-lg">
-                  {message.message}
-                </div>
-              </div>
-            ))}
-          </div>
-
-          {/* // map through receiverMessages */}
-          {receiverMessages.map((message) => (
-            <div key={message.sender}>
-              <h1 className="text-lg font-bold">{message.sender}</h1>
-              <div className="flex w-[91px] items-start justify-start gap-2 rounded-bl-[20px] rounded-br-[20px] rounded-tr-[20px] bg-amber-200 p-3 px-[15px] py-2.5 ">
-                {message.message}
-              </div>
-            </div>
-          ))}
+          {messages.map((message) =>
+            isSender(message.sender) ? (
+              <SenderChatMessage key={message.message} message={message} />
+            ) : (
+              <ReceiverChatMessage key={message.message} message={message} />
+            ),
+          )}
         </div>
       </section>
 
       {/* Input section */}
       <section className="flex items-center justify-center px-4">
-        <div className="flex w-full max-w-2xl items-center justify-between py-2 pl-4 pr-2 ">
+        <form
+          onSubmit={handleSubmit}
+          className="flex w-full items-center justify-between py-2 pl-4 pr-2 "
+        >
           <Smile className="relative mx-3 h-8 w-8" />
           <input
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
             type="text"
             placeholder="Reply..."
             className="w-full max-w-xl rounded-full px-6 py-3  focus:outline-none focus:ring-2 "
@@ -94,20 +97,29 @@ export default function ChatPage() {
           <Button className=" mx-3 rounded-full bg-primary p-2">
             <ChevronRight className="h-6 w-6 text-white" />
           </Button>
-        </div>
+        </form>
       </section>
     </div>
   );
 }
 
-{
-  /* <div className="flex  items-start justify-start gap-[5px] ">
-          <h1 className="text-lg font-bold ">Jenny</h1>
-        </div>
-        <div className="inline-flex h-[35px] w-[91px] items-start justify-start gap-2 rounded-bl-[20px] rounded-br-[20px] rounded-tr-[20px] bg-amber-200 px-[15px] py-2.5">
-          <div className="h-[15px] w-[15px] rounded-full bg-amber-300" />
-          <div className="h-[15px] w-[15px] rounded-full bg-amber-300" />
-          <div className="h-[15px] w-[15px] rounded-full bg-amber-300" />
-        </div>
-      </section> */
+function SenderChatMessage({ message }: { message: ChatMessage }) {
+  return (
+    <div className="flex items-end justify-end">
+      <div className="w-fit max-w-xs break-words rounded-3xl rounded-tr-none bg-secondary p-3">
+        {message.message}
+      </div>
+    </div>
+  );
+}
+
+function ReceiverChatMessage({ message }: { message: ChatMessage }) {
+  return (
+    <div>
+      <h1 className="text-lg font-bold">{message.sender}</h1>
+      <div className="flex w-fit max-w-xs items-start justify-start gap-2 rounded-3xl rounded-tl-none bg-[#FFD78A] p-3 px-[15px] py-2.5">
+        {message.message}
+      </div>
+    </div>
+  );
 }
