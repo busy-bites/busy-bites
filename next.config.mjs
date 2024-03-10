2940e94364cce50f0345cea72a410011248126ee
scottchen98 <scott.chen1117@gmail.com>
2024-03-09 23:25:35 -0800

    update next.config.js with pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  reloadOnOnline: true,
+});
+
 const nextConfig = {};
 
-export default nextConfig;
+module.exports =
+  process.env.NODE_ENV === "production" ? withPWA(nextConfig) : nextConfig;
