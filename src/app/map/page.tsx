eedd400fe0dc53980b4f5fe0c4fb2e1bb9ddca66
scottchen98 <scott.chen1117@gmail.com>
2024-03-09 16:49:08 -0800

    display pin and carousel

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -5,10 +5,17 @@ import { MapContainer, Marker, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 import { useRef } from "react";
+import { MapOverlayCarousel } from "./map-overlay-carousel";
 
 const ICON = icon({
   iconUrl: "/marker.png",
   iconSize: [32, 32],
+});
+
+const PIN = icon({
+  iconUrl: "/pin.png",
+  iconSize: [60, 60],
+  iconAnchor: [18, 51],
 });
 
 export default function MapPage() {
@@ -17,15 +24,23 @@ export default function MapPage() {
   const longitude = -123.0034;
 
   return (
-    <MapContainer
-      center={[latitude, longitude]}
-      zoom={17}
-      ref={mapRef}
-      style={{ height: "calc(100dvh - 56px)", width: "100%" }}
-    >
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker position={[latitude, longitude]} icon={ICON}></Marker>
-      <Marker position={[49.252, -123.0019]} icon={ICON}></Marker>
-    </MapContainer>
+    <div className="relative">
+      <MapContainer
+        center={[latitude, longitude]}
+        zoom={17}
+        ref={mapRef}
+        style={{
+          height: "calc(100dvh - 56px)",
+          width: "100%",
+          zIndex: -5,
+        }}
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Marker position={[latitude, longitude]} icon={ICON}></Marker>
+        <Marker position={[49.252, -123.0019]} icon={ICON}></Marker>
+        <Marker position={[latitude, longitude]} icon={PIN}></Marker>
+      </MapContainer>
+      <MapOverlayCarousel />
+    </div>
   );
 }
