dd3b297ce3dbce36e5426bdc60b64e4d10698dfe
scottchen98 <scott.chen1117@gmail.com>
2024-03-09 17:14:32 -0800

    update pin location functionality

--- src/app/map/map-overlay-carousel.tsx
+++ src/app/map/map-overlay-carousel.tsx
@@ -6,41 +6,33 @@ import {
   CarouselItem,
 } from "@/components/ui/carousel";
 import MapOverlay from "@/components/design/map-overlay";
-import SandwichIcon from "@/components/icons/SandwichIcon";
 
-const mapOverlayItems = [
-  {
-    amount: "5",
-    image: <SandwichIcon width={57.5} height={57.5} />,
-    food: "Ramen",
-    host: "Jenny",
-    distance: "1",
-    time: "3pm",
-  },
-  {
-    amount: "3",
-    image: <SandwichIcon width={57.5} height={57.5} />,
-    food: "Sandwich",
-    host: "Sia",
-    distance: "1.5",
-    time: "4pm",
-  },
-  {
-    amount: "2",
-    image: <SandwichIcon width={57.5} height={57.5} />,
-    food: "Milk",
-    host: "Joyce",
-    distance: "1.3",
-    time: "5pm",
-  },
-];
-
-export function MapOverlayCarousel() {
+export function MapOverlayCarousel({
+  mapOverlayItems,
+  setCurrentPinLocation,
+}: {
+  mapOverlayItems: {
+    amount: string;
+    image: React.ReactNode;
+    food: string;
+    host: string;
+    distance: string;
+    time: string;
+    coordinates: { lat: number; lng: number };
+  }[];
+  setCurrentPinLocation: React.Dispatch<
+    React.SetStateAction<{ lat: number; lng: number }>
+  >;
+}) {
   return (
     <Carousel className="absolute bottom-8 w-full">
       <CarouselContent className="ml-5">
         {mapOverlayItems.map((item, index) => (
-          <CarouselItem key={index} className="basis-[270px] pl-1">
+          <CarouselItem
+            key={index}
+            className="basis-[270px] pl-1"
+            onClick={() => setCurrentPinLocation(item.coordinates)}
+          >
             <MapOverlay {...item} />
           </CarouselItem>
         ))}
